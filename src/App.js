dbc10fda2332a54574b19a5d95187e1470d63831
naveen <neonew8984@gmail.com>
2021-06-07 02:21:00 +0545

    HangMan in React

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
 import Popup from './components/Popup';
 import Notification from './components/Notification';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const words = ['application', 'programming', 'interface', 'wizard'];
 
@@ -17,12 +17,38 @@ function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeydown);
+
+     return () => window.removeEventListener('keydown', handleKeydown);
+  })
+
   return (
     <>
       <Header />
       <div className='game-container'>
         <Figure />
-        <WrongLetters />
+        <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
       <Popup />
