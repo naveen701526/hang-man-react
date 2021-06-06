797d0c9d4bcdaf2e62f04813650aea13875dd7f3
naveen <neonew8984@gmail.com>
2021-06-07 01:12:31 +0545

    HangMan in React

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+         <h1>Hang Man</h1>   
+         <p>Find the Hidden word - Enter a letter</p>
+        </>
+    )
+}
+
+export default Header
