f39c072afc75f04ec4523f9b806a5084f4a9a40b
naveen <neonew8984@gmail.com>
2021-06-07 02:30:39 +0545

    HangMan in React

--- src/components/WrongLetters.js
+++ src/components/WrongLetters.js
@@ -5,7 +5,7 @@ const WrongLetters = ({wrongLetters}) => {
     <div class='wrong-letters-container'>
       <div>
         {wrongLetters.length > 0 && <p>Wrong</p>}
-        {wrongLetters.map((letter, index) => <span key={index}>${letter}</span>)
+        {wrongLetters.map((letter, index) => <span key={index}>{letter}</span>)
         .reduce((prev, curr) => prev === null ? [curr] : [prev, ', ', [curr]], null)}
       </div>
     </div>
